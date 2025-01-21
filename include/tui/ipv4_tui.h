7d2396a36c014bb185e473579c053304830a5d67
Charly Alizadeh <alizadehcharly@gmail.com>
2025-01-22 00:51:16 +0100

    refactor: remove unused `ipv4tui_t` struct

--- include/tui/ipv4_tui.h
+++ include/tui/ipv4_tui.h
@@ -7,17 +7,6 @@
 #include "../ipv4/ipv4.h"
 
 
-typedef struct {
-    WINDOW* window;
-    unsigned long long values[12];
-    char** labels;
-    int* bitlen;
-    size_t row, col;
-    enum InputMode mode;
-    int split;
-    size_t current;
-} ipv4tui_state_t;
-
 void ipv4_tui(WINDOW *window, uint8_t *bytes);
 
 
